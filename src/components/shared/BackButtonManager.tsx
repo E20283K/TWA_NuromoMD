620bba0c188cbf5b407079909c57e4bfc67cc836
E20283K <ezoz2497@gmail.com>
2026-04-29 20:45:28 +0500

    fix: prevent app crash when BackButton is undefined in BackButtonManager

--- src/components/shared/BackButtonManager.tsx
+++ src/components/shared/BackButtonManager.tsx
@@ -11,15 +11,31 @@ export const BackButtonManager: React.FC = () => {
       navigate(-1);
     };
 
-    if (location.pathname !== '/') {
-      tg.BackButton.show();
-      tg.BackButton.onClick(handleBack);
-    } else {
-      tg.BackButton.hide();
+    if (tg && tg.BackButton) {
+      if (location.pathname !== '/') {
+        try {
+          tg.BackButton.show();
+          tg.BackButton.onClick(handleBack);
+        } catch (e) {
+          console.error("Error showing back button", e);
+        }
+      } else {
+        try {
+          tg.BackButton.hide();
+        } catch (e) {
+          console.error("Error hiding back button", e);
+        }
+      }
     }
 
     return () => {
-      tg.BackButton.offClick(handleBack);
+      if (tg && tg.BackButton) {
+        try {
+          tg.BackButton.offClick(handleBack);
+        } catch (e) {
+          console.error("Error removing back button click", e);
+        }
+      }
     };
   }, [location.pathname, navigate]);
 
